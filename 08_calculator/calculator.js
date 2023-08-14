ab0204d968dc43dcf62de500cec4ac0412594629
Faith <faithmusangi50@gmail.com>
2023-08-14 11:12:25 +0300

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current);
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  // return a * b;
+  return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0) return 1;
+	let product = 1;
+  for (let i = n; i > 0; i--){
+    product *= 1;
+  }
 };
 
 // Do not edit below this line
